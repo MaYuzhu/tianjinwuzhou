755561610f49c3152078c2786f25995eadfa4ca7
MaYuzhu <mayuzhu123@sina.com>
2018-09-21 09:08:12 +0800

    ui图和页面

---
+++ TianjinWuzhouPort/js/fence.js
@@ -0,0 +1,48 @@
+(function (w) {
+    //选择时间范围input
+    laydate.render({
+        elem: '#weilan_add'
+        ,type: 'datetime'
+        ,range: '~'
+        ,format: 'M/d/H:m'
+    });
+    //翻页三角
+    $('footer').on('click',function (ev) {
+        var $target = $(ev.target);
+        if($target.prop("nodeName") == "DIV"){
+            $('footer>div').removeClass('on4')
+            $target.addClass("on4");
+        }
+    })
+    //拖动
+    $(".show").mousedown(function(e){ //e鼠标事件
+        $(this).css("cursor","move");//改变鼠标指针的形状
+
+        var offset = $(this).offset();//DIV在页面的位置
+        var x = e.pageX - offset.left;//获得鼠标指针离DIV元素左边界的距离
+        var y = e.pageY - offset.top;//获得鼠标指针离DIV元素上边界的距离
+        $(document).bind("mousemove",function(ev){ //绑定鼠标的移动事件，因为光标在DIV元素外面也要有效果，所以要用doucment的事件，而不用DIV元素的事件
+            $(".show").stop();//加上这个之后
+
+            var _x = ev.pageX - x;//获得X轴方向移动的值
+            var _y = ev.pageY - y;//获得Y轴方向移动的值
+            if(_x<0){
+                _x = 0
+            }else if(_x > $(document).width() - ($('.show').width() + 4)){
+                _x = $(document).width() - ($('.show').width() + 4)
+            }
+            if(_y<0){
+                _y = 0
+            }else if(_y > $(document).height() - ($('.show').height() + 4)){
+                _y = $(document).height() - ($('.show').height() + 4)
+            }
+            $(".show").animate({left:_x+"px",top:_y+"px"},5);
+        });
+    });
+
+    $(document).mouseup(function(){
+        //$(".show").css("cursor","default");
+        $(".show").css("cursor","move");
+        $(this).unbind("mousemove");
+    });
+})(window)
--- TianjinWuzhouPort/js/index.js
+++ TianjinWuzhouPort/js/index.js
@@ -1,3 +1,81 @@
 (function (w) {
+    //在线监测与轨迹切换
+    let $tab = $('.right_content>:nth-child(1)>p')
+    $tab.on('click',(function() {
+        var i = $(this).index();
+        $(this).addClass('on').siblings().removeClass('on');
+            $('.on img').css('display','block')
+            $('.box_right>div').eq(i).show().siblings().hide();
+        })
+    );
+    //js添加拖车li
+    for(let i=0;i<12;i++){
+        $('.list_che').append(`<li>
+                                <input type="checkbox" value="" id="myCheck+${i}" class="myCheck">
+                                <label for="myCheck+${i}"></label>
+                                <p>拖车A1233451</p>
+                                <div></div>
+                            </li>`)
+    }
+    for(let i=0;i<5;i++){
+        $('.list_che2').append(`<li>
+                                <input type="checkbox" value="" id="myCheck2+${i}" class="myCheck">
+                                <label for="myCheck2+${i}"></label>
+                                <p>拖车A1233451</p>
+                                <div></div>
+                            </li>`)
+    }
+    //翻页三角
+    $('.next_wrap').on('click',function (ev) {
+        var $target = $(ev.target);
+        if($target.prop("nodeName") == "LI"){
+            $('.next_wrap>li').removeClass('on2')
+            $target.addClass("on2");
+        }
+    })
+    $('.next_wrap3').on('click',function (ev) {
+        var $target = $(ev.target);
+        if($target.prop("nodeName") == "LI"){
+            $('.next_wrap3>li').removeClass('on3')
+            $target.addClass("on3");
+        }
+    })
+    //移入右侧显示
+    $('.right').on('mouseover',function () {
+        $('.right_content').css('transform', 'translateX(0)')
+    })
+    //点击三角收回
+    $('.right_hide').on('click',function () {
+        $('.right_content').css('transform', 'translateX(230px)')
+    })
 
+    laydate.render({
+        elem: '#one_guiji'
+        ,type: 'datetime'
+        ,range: '~'
+        ,format: 'M/d/H:m'
+    });
+    /*laydate.render({
+        elem: '#test16'
+        ,type: 'datetime'
+        ,range: '到'
+        ,format: 'yyyy年M月d日H时m分s秒'
+    });*/
+    //播放按钮
+    var flag = true
+    $('#play_2').on('click',function () {
+        if(flag){
+            $('#play_2>span').css({ 'background': 'url("./images/zanting.png") no-repeat left top',
+                                    'background-size':'100% 100%',
+                                    'margin': '11px 10px'
+                                    })
+            flag = false
+        }else if(!flag){
+            $('#play_2>span').css({ 'background': 'url("./images/play_but.png") no-repeat left top',
+                                    'margin': '11px 15px'
+            })
+            flag = true
+        }
+    })
+    
 })(window)
